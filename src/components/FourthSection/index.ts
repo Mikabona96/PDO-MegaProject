eca8c345039d609710b99421a8a0c2c3c1926834
Mikabona96 <76862391+Mikabona96@users.noreply.github.com>
2022-11-24 17:29:58 +0200

    4th section form done

--- src/components/FourthSection/index.ts
+++ src/components/FourthSection/index.ts
@@ -1,7 +1,61 @@
 import '../../assets/img/4th-section-bg.png';
 
 export const fourthSectionFunction = () => {
-    console.log('>>>fourthSectionFunction <<<');
+    const data = {
+        name:         '',
+        phone:        '',
+        email:        '',
+        comment:      '',
+        setToInitial: function () {
+            this.name = '';
+            this.email = '';
+            this.phone = '';
+            this.comment = '';
+        },
+    };
+
+    const name = document.querySelector('.fourthsection .form-wrapper .form .name');
+    const tel = document.querySelector('.fourthsection .form-wrapper .form .tel');
+    const email = document.querySelector('.fourthsection .form-wrapper .form .email');
+    const comment = document.querySelector('.fourthsection .form-wrapper .form .comment');
+    const btn = document.querySelector('.fourthsection .form-wrapper .form .btn');
+    const form = (document.querySelector('.fourthsection .form-wrapper .form')) as HTMLFormElement;
+    name?.addEventListener('input', (event) => {
+        const target = (event.target) as HTMLInputElement;
+        data.name = target.value;
+        target.value = data.name;
+    });
+    tel?.addEventListener('input', (event) => {
+        const target = (event.target) as HTMLInputElement;
+        data.phone = target.value;
+        target.value = data.phone;
+    });
+    email?.addEventListener('input', (event) => {
+        const target = (event.target) as HTMLInputElement;
+        data.email = target.value;
+        target.value = data.email;
+    });
+    comment?.addEventListener('input', (event) => {
+        const target = (event.target) as HTMLInputElement;
+        data.comment = target.value;
+        target.value = data.comment;
+        console.log(data);
+    });
+
+    btn?.addEventListener('click', (event) => {
+        event.preventDefault();
+        // fetch('https://fake.api.com', {
+        //     method:  'POST',
+        //     headers: {
+        //         'Content-type': 'application/json',
+        //     },
+        //     body: JSON.stringify(data),
+        // });
+
+        setTimeout(() => {
+            form.reset();
+        }, 1000);
+    });
 };
 
 fourthSectionFunction();
